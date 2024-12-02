1294b7ab7943f2dc1175f2d43f4764d86e713e7b
Giovana <giovanamatiascampos9@gmail.com>
2024-12-02 10:02:37 -0300

    Aprendi o que é uma variável

---
+++ Aula 2/Aula02.py
@@ -0,0 +1,6 @@
+print('bom dia a todos!')
+nome_usuario = 'giovana' #tiopo-> str = string
+idade_usuario = 22
+altura_usuario = 1.75
+maior_de_idade =True#tipo -> bool = boolean
+print('Seja bem-vindo',nome_usuario,'sua idade é',idade_usuario,'sua altura é',altura_usuario,'você é maior de idade?',maior_de_idade)
